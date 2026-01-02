673a915a718b2bfb5059de737cce632a7578c0ff
ChampionAgy <richowusumond@gmail.com>
2026-01-02 07:26:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ model = joblib.load("irrigation_svm_model.pkl")
 
 # New sample (temperature, humidity, moisture)
 
-# temperature = 0
-# humidiity = 0
-# soil_moisture = 0
-
-# sample = pd.DataFrame([{
-#     "temperature": temperature,
-#     "humidity": humidity,
-#     "moisture": soil_moisture
-# }])
+temperature = 0
+humidiity = 0
+soil_moisture = 0
 
 @app.post('/predict')
 async def get_humid(value: Request):
@@ -47,6 +41,26 @@ async def get_humid(value: Request):
     print("prediction", prediction)
 
     return {"label": bool(prediction)}
+
+
+@app.post('/update')
+async def update(request: Request):
+    data = await request.json()
+    temperature = data.get("temperature")
+    humidity = data.get("humidity")
+    soil_moisture = data.get("moisture")
+
+    sample_data["temperature"] = temperature
+    sample_data["humidity"] = humidity
+    sample_data["moisture"] = soil_moisture
+    return {"response": "updated"}
+
+
+@app.get('/stream')
+async def stream():
+    return {"data": sample_data}
+    
+
 
 
 if __name__=='__main__':
